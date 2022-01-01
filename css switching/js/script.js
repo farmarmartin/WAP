e085aa30e9add86e55a3bf98a6d747f048bd29bb
martin job <martin.job2906@gmail.com>
2022-01-01 23:28:08 +0100

    update

--- css switching/js/script.js
+++ css switching/js/script.js
@@ -1,22 +1,10 @@
 let path = document.getElementById("source");
 let i = 2;
+
 document.getElementById("btn").onclick = function() {switch_css()};
 function switch_css(){
-    
-    if(i == 1){
-        path.href = "css/style1.css";
-        i = 2;
-    }
-    else if( i == 2){
-        path.href = "css/style2.css";
-        i = 3;
-    }
-    else if(i == 3){
-        path.href = "css/style3.css";
-        i = 4;
-    }
-    else{
-        path.href = "css/style4.css";
-        i = 1;
-    }
+    i == 1 ? (path.href = "css/style1.css", i = 2) : 
+    i == 2 ? (path.href = "css/style2.css", i = 3) :
+    i == 3 ? (path.href = "css/style3.css", i = 4) :
+    i == 4 ? (path.href = "css/style4.css", i = 1) : null;
 }
