e9f3c04bb3f41fee5372032849a4ba5abad8c50b
martin job <martin.job2906@gmail.com>
2022-02-28 21:42:25 +0100

    update

--- phpwatch/index.php
+++ phpwatch/index.php
@@ -6,15 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hodiny</title>
 </head>
-<body style="font-family: monospace; color:darkcyan; background: black;" >
+<body style="font-family: monospace; font-size: 1em; color:darkcyan; background: black;" >
     <h1>Datum a čas</h1>
     <?php
         date_default_timezone_set('CET');
-        echo date("Y.m.d  H:i:s");
+        trDay();
+        echo date(" d.");
+        trMonth();
+        echo date(" Y H:i:s");
         $hour = date("H");
         $min = date("i");
         $sec = date("s");
-
+        
         echo "<ul>";
         echo "<li>H:&nbsp";
             for ($i = 0; $i < $hour; $i++) {
@@ -32,8 +35,35 @@
             }
         echo "</li>";
         echo "</ul>";
-       
 
+        function trDay(){
+            $weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+            $weekCs = ['pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle'];
+            $den = date("l");
+
+            for ($i = 0; $i < 7; $i++){
+                    if ($den == $weekEn[$i]){
+                    $den = $weekCs[$i];
+                }
+            }
+            echo $den;
+        }
+
+        function trMonth(){
+            $monthEn = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            $monthCs = ['ledna', 'února', 'března', 'dubna', 'května', 'června', 'července', 'srpna', 'září', 'října', 'listopadu', 'prosince'];
+            $month = date("F");
+
+            for ($i = 0; $i < 12; $i++){
+                    if ($month == $monthEn[$i]){
+                    $month = $monthCs[$i];
+                }
+            }
+            echo $month;
+        }
+
+        $page = $_SERVER['PHP_SELF'];
+        header("Refresh: 1; url=$page");
     ?>
 </body>
 </html>
